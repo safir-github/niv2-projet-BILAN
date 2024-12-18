59f24e55f71e8999461a28a5f270fb84ffeb1673
safir-github <safirmuscu@gmail.com>
2024-12-18 22:35:33 +0100

    Création du dossier 'environments' pour gérer l'adresse électronique pour l'envoie du mail.

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'; // Import de l'environnement
+
 
 @Injectable({
   providedIn: 'root' // Rend ce service disponible dans toute l'application
 })
 export class ContactService {
-  private apiUrl = 'http://localhost:3001/send-email'; // URL de l'API pour l'envoi de l'email
+  private apiUrl = environment.apiUrl; // Utilisation de l'URL définie dans l'environnement
 
   constructor(private http: HttpClient) {}
 
-  // Méthode pour envoyer un message via l'API
+  // Méthode pour envoyer un message à l'API
   sendMessage(data: { name: string; subject: string; message: string }): Observable<any> {
     return this.http.post(this.apiUrl, data); // Envoi des données au backend
   }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3001/send-email' // URL de l'API pour le développement
+  };
+  
