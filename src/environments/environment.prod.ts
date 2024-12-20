3c918ff8916be38a3f8cf842decec2a44ccf3d93
safir-github <safirmuscu@gmail.com>
2024-12-20 11:05:10 +0100

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://api.example.com/send-email' // URL de l'API pour la production
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3001/send-email' // URL de l'API pour le développement
-  };
-  
